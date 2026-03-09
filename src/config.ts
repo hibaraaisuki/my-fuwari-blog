e70421fd486f80096c045d6a1a0cccea6cfbdb4f
saiki-LAPOTOP-33IH30OJ <443577815@qq.com>
2026-03-09 17:17:57 +0800

    调试：修改个人链接

--- src/config.ts
+++ src/config.ts
@@ -46,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/wjnstudy/my-fuwari-blog", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/wjnstudy", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
@@ -58,21 +58,21 @@ export const profileConfig: ProfileConfig = {
 	bio: "Saiki的个人博客网站，正在建设中。",
 	links: [
 		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+			name: "Bilibili",
+			icon: "fa6-brands:bilibili", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
+			url: "https://space.bilibili.com/102488269?spm_id_from=333.1007.0.0",
 		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
-		},
+		// {
+		// 	name: "Steam",
+		// 	icon: "fa6-brands:steam",
+		// 	url: "https://store.steampowered.com",
+		// },
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/wjnstudy/my-fuwari-blog",
 		},
 	],
 };
