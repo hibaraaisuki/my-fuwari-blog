749057e7b70c0cae65d76375972a829815033841
saiki-LAPOTOP-33IH30OJ <443577815@qq.com>
2026-03-10 15:08:41 +0800

    调整：修改了网站描述。

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Saiki",
-	subtitle: "Personal Blog",
+	subtitle: "Saiki的个人博客网站",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 55, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
